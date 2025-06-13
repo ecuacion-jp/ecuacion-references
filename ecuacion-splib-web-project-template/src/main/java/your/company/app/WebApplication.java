ff0f98136691f4ab502f9306989254e4b67d1ddb
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-06-14 08:15:18 +0900

    ecuacion-modules updated.

--- ecuacion-splib-web-project-template/src/main/java/your/company/app/WebApplication.java
+++ ecuacion-splib-web-project-template/src/main/java/your/company/app/WebApplication.java
@@ -29,7 +29,7 @@ public class WebApplication extends SpringBootServletInitializer {
     SplibModelAttributes atr = new SplibModelAttributes();
     atr.setBsBgGradient(true);
     atr.setShowsMessagesLinkedToItemsAtTheTop(true);
-    atr.setShowsMessagesLinkedToItemsAtEachField(false);
+    atr.setShowsMessagesLinkedToItemsAtEachItem(false);
     
     return atr;
   }
