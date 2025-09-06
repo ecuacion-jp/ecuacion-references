7c5b48bc3a998de9952c3e43c110dfed4c3bd833
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2025-09-06 14:24:35 +0900

    Updated.

--- ecuacion-references-splib-web-tutorial/src/main/java/jp/ecuacion/referenceapps/splib/web/tutorial/domain/system/StCommonFeaturesController.java
+++ ecuacion-references-splib-web-tutorial/src/main/java/jp/ecuacion/referenceapps/splib/web/tutorial/domain/system/StCommonFeaturesController.java
@@ -1,39 +1,68 @@
-//package jp.ecuacion.referenceapps.splib.web.tutorial.domain.system;
-//
-//import jp.ecuacion.lib.core.exception.checked.AppException;
-//import jp.ecuacion.splib.web.controller.SplibGeneralController;
-//import jp.ecuacion.splib.web.service.SplibGeneralDoNothingService;
-//import org.springframework.context.annotation.Scope;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@Scope("prototype")
-//@RequestMapping("/public/stCommonFeatures")
-//public class StCommonFeaturesController
-//    extends SplibGeneralController<SplibGeneralDoNothingService> {
-//
-//  public StCommonFeaturesController() {
-//    super("stCommonFeatures");
-//  }
-//
-//  @GetMapping("page")
-//  public String page(Model model) throws AppException {
-//    prepare(model, new StCommonFeaturesForm());
-//    return getDefaultHtmlPageName();
-//  }
-//
-//  @GetMapping(value = "action", params = "404")
-//  public String method404(Model model) {
-//
-//    return "redirect:/public/notFound/page";
-//  }
-//
-//  @GetMapping(value = "action", params = "accessDenied")
-//  public String config(Model model) {
-//
-//    return "redirect:/account/loginHome/page";
-//  }
-//}
+package jp.ecuacion.referenceapps.splib.web.tutorial.domain.system;
+
+import jakarta.validation.Valid;
+import jp.ecuacion.lib.core.exception.checked.AppException;
+import jp.ecuacion.splib.core.form.record.SplibRecord;
+import jp.ecuacion.splib.web.bean.HtmlItem;
+import jp.ecuacion.splib.web.controller.SplibGeneralController;
+import jp.ecuacion.splib.web.form.SplibGeneralForm;
+import jp.ecuacion.splib.web.form.record.RecordInterface;
+import jp.ecuacion.splib.web.service.SplibGeneralDoNothingService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
+@RequestMapping("/public/03-system/stCommonFeatures")
+public class StCommonFeaturesController
+    extends SplibGeneralController<SplibGeneralDoNothingService> {
+
+  public StCommonFeaturesController() {
+    super("stCommonFeatures", newContext().functionKinds("03-system"));
+  }
+
+  @GetMapping("page")
+  public String page(Model model) throws AppException {
+    prepare(model, new StCommonFeaturesForm());
+    return getDefaultHtmlPageName();
+  }
+
+  @GetMapping(value = "action", params = "404")
+  public String method404(Model model) {
+
+    return "redirect:/public/notFound/page";
+  }
+
+  @GetMapping(value = "action", params = "accessDenied")
+  public String config(Model model) {
+
+    return "redirect:/account/loginHome/page";
+  }
+  
+  public static class StCommonFeaturesForm extends SplibGeneralForm {
+
+    @Valid
+    private StCommonFeaturesRecord stCommonFunctions = new StCommonFeaturesRecord();
+
+    public StCommonFeaturesRecord getStCommonFunctions() {
+      return stCommonFunctions;
+    }
+
+    public void setStCommonFunctions(StCommonFeaturesRecord stCommonFunctions) {
+      this.stCommonFunctions = stCommonFunctions;
+    }
+  }
+  
+  public static class StCommonFeaturesRecord extends SplibRecord implements RecordInterface {
+
+    @Override
+    public HtmlItem[] getHtmlItems() {
+      return new HtmlItem[] {};
+    }
+
+  }
+
+}
--- ecuacion-references-splib-web-tutorial/src/main/java/jp/ecuacion/referenceapps/splib/web/tutorial/domain/system/StPagesProvidedByLibraryController.java
+++ ecuacion-references-splib-web-tutorial/src/main/java/jp/ecuacion/referenceapps/splib/web/tutorial/domain/system/StPagesProvidedByLibraryController.java
@@ -1,38 +1,67 @@
-//package jp.ecuacion.referenceapps.splib.web.tutorial.domain.system;
-//
-//import jp.ecuacion.splib.web.controller.SplibGeneralController;
-//import jp.ecuacion.splib.web.service.SplibGeneralDoNothingService;
-//import org.springframework.context.annotation.Scope;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@Scope("prototype")
-//@RequestMapping("/public/stPagesProvidedByLibrary")
-//public class StPagesProvidedByLibraryController
-//    extends SplibGeneralController<SplibGeneralDoNothingService> {
-//
-//  public StPagesProvidedByLibraryController() {
-//    super("stPagesProvidedByLibrary");
-//  }
-//
-//  @GetMapping("page")
-//  public String page(Model model) {
-//    model.addAttribute("stPagesProvidedByLibraryForm", new StPagesProvidedByLibraryForm());
-//    return getDefaultHtmlPageName();
-//  }
-//
-//  @GetMapping(value = "action", params = "systemError")
-//  public String systemError(Model model) {
-//
-//    return "error";
-//  }
-//
-//  @GetMapping(value = "action", params = "config")
-//  public String config(Model model) {
-//
-//    return "redirect:/ecuacion/public/config/page";
-//  }
-//}
+package jp.ecuacion.referenceapps.splib.web.tutorial.domain.system;
+
+import jakarta.validation.Valid;
+import jp.ecuacion.splib.core.form.record.SplibRecord;
+import jp.ecuacion.splib.web.bean.HtmlItem;
+import jp.ecuacion.splib.web.controller.SplibGeneralController;
+import jp.ecuacion.splib.web.form.SplibGeneralForm;
+import jp.ecuacion.splib.web.form.record.RecordInterface;
+import jp.ecuacion.splib.web.service.SplibGeneralDoNothingService;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
+@RequestMapping("/public/03-system/stPagesProvidedByLibrary")
+public class StPagesProvidedByLibraryController
+    extends SplibGeneralController<SplibGeneralDoNothingService> {
+
+  public StPagesProvidedByLibraryController() {
+    super("stPagesProvidedByLibrary", newContext().functionKinds("03-system"));
+  }
+
+  @GetMapping("page")
+  public String page(Model model) {
+    model.addAttribute("stPagesProvidedByLibraryForm", new StPagesProvidedByLibraryForm());
+    return getDefaultHtmlPageName();
+  }
+
+  @GetMapping(value = "action", params = "systemError")
+  public String systemError(Model model) {
+
+    return "error";
+  }
+
+  @GetMapping(value = "action", params = "config")
+  public String config(Model model) {
+
+    return "redirect:/ecuacion/public/config/page";
+  }
+  
+  public static class StPagesProvidedByLibraryForm extends SplibGeneralForm {
+
+    @Valid
+    private StPagesProvidedByLibraryRecord stPagesProvidedByLibrary =
+        new StPagesProvidedByLibraryRecord();
+
+    public StPagesProvidedByLibraryRecord getStPagesProvidedByLibrary() {
+      return stPagesProvidedByLibrary;
+    }
+
+    public void setStPagesProvidedByLibrary(StPagesProvidedByLibraryRecord stPagesProvidedByLibrary) {
+      this.stPagesProvidedByLibrary = stPagesProvidedByLibrary;
+    }
+  }
+  
+  public static class StPagesProvidedByLibraryRecord extends SplibRecord implements RecordInterface {
+
+    @Override
+    public HtmlItem[] getHtmlItems() {
+      return new HtmlItem[] {};
+    }
+
+  }
+}
