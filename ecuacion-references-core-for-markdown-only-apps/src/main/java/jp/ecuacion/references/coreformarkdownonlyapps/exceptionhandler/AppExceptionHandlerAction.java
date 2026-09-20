7ea5800df5d16315b5dbd668e24c8a8483d8cc08
Yosuke Tanaka <yosuke.tanaka@ecuacion.jp>
2026-09-21 07:20:06 +0900

    ActionOnThrowable renamed to AppExceptionHandlerAction

--- ecuacion-references-core-for-markdown-only-apps/src/main/java/jp/ecuacion/references/coreformarkdownonlyapps/exceptionhandler/AppExceptionHandlerAction.java
+++ ecuacion-references-core-for-markdown-only-apps/src/main/java/jp/ecuacion/references/coreformarkdownonlyapps/exceptionhandler/AppExceptionHandlerAction.java
@@ -21,7 +21,7 @@ import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ActionOnThrowable implements SplibExceptionHandlerAction {
+public class AppExceptionHandlerAction implements SplibExceptionHandlerAction {
 
   @Override
   public void execute(@NonNull Throwable th) {
